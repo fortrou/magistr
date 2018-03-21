dc835dd2284253be2ed4eb827eb69c3cfb0978f4
DESKTOP-BVMM1CB\fortrou <fortrou@gmail.com>
2018-03-21 17:06:57 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,658 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+	<meta charset="UTF-8">
+
+	<title>МагістрЗНО - підготовка до вступу до магістратури - Головна</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes">
+
+	<link rel="stylesheet" href="tpl_css/fontawesome/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="tpl_css/bootstrap/bootstrap.css">
+
+	<link rel="stylesheet" href="tpl_css/stylesheet.css">
+
+	<script type="text/javascript" src="tpl_js/jquery.min.js"></script>
+
+	<script type="text/javascript" src="tpl_js/main-js.js"></script>
+
+</head>
+
+<body>
+
+	<div class="container">
+
+		<div class="row">
+
+			<div class="header">
+
+				<div class="header-info">
+					<div class="content">
+
+						<div class="col-md-12">
+
+							<a href="tel: +380996545428"><span class="fa fa-phone"></span><span class="contacts-text hide-480">(099) 654-54-28</span></a>
+
+
+							<a href=""><span class="fa fa-envelope"></span><span class="contacts-text hide-768">magistr.zno@gmail.com</span></a>
+
+							<a href="" class="hide-960"><span class="fa fa-calendar"></span><span class="contacts-text ">ПН-ПТ: 11.00 - 19.00</span></a>
+
+							<a href=""
+
+							class="block-display float-right-block button-full-yellow element-width-70 element-border-radius-5 element-margin-left-10">
+
+							ВХІД
+
+						</a>
+
+						<a href=""
+
+						class="block-display float-right-block button-border-yellow element-width-160 element-border-radius-5 ">
+
+						РЕЄСТРАЦІЯ
+
+					</a>
+
+				</div>
+
+			</div>
+		</div>
+
+		<div class="header-begin-info">
+			<div class="content">
+
+				<div class="col-md-12">
+
+					<img src="/tpl_img/logo.png" alt="" class="block-display float-left-block logo">
+
+
+				<div class="header-begin-info-btns float-right-block">
+					<a href="" 
+					class="block-display button-border-blue element-width-240 element-border-radius-5 ">
+					СПРОБУВАТИ БЕЗКОШТОВНО
+					</a>
+
+					<a href="" 
+					class="block-display button-full-blue element-width-160 element-border-radius-5 element-margin-left-10">
+					ПОЧАТИ НАВЧАННЯ
+					</a>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+<div class="clear"></div>
+
+<div class="menu">
+	<div class="content">
+
+		<ul class="menu-elements">
+
+			<li data-rel-section="1">ГОЛОВНЕ</li>
+
+			<li data-rel-section="2">ЯК РОЗПОЧАТИ</li>
+
+			<li data-rel-section="3">ЯК НАВЧАТИСЯ</li>
+
+			<li data-rel-section="4">ЧОМУ МИ?</li>
+
+		</ul>
+
+		<div class="clear"></div>
+
+	</div>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+<div class="container">
+
+	<div class="row">
+		<div class="content">
+
+			<div class="main-wrapper">
+
+				<div class="main sub-section" data-section="1">
+
+					<div class="sub-section-element">
+
+						<div class="info-block col-md-12 ">
+
+							<div>
+
+								Онлайн-центр <span class="text-magistr">"МАГІСТР</span><span class="text-zno">ЗНО"</span> готує до вступу до магістратури за спеціальністю <span class="text-magistr">"ПРАВО"</span>.
+
+							</div>
+
+							<div>
+
+								Ми пропонуємо підготовку до іспитів з трьох дисциплін. 
+
+							</div>
+
+						</div>
+
+						<div class="elements-block col-lg-10 offset-lg-1 col-md-12 offset-md-0 col-sm-12 offset-sm-0">
+
+							<div class="element-wrap">
+
+								<img src="/tpl_img/femida.png" class="main-image" alt="">
+
+								<div>
+
+									<p class="subject-title element-margin-top-20">ПРАВО</p>
+
+								</div>
+
+							</div>
+
+							<div class="element-wrap">
+
+								<img src="/tpl_img/english.png" class="main-image" alt="">
+
+								<div>
+
+									<p class="subject-title element-margin-top-20">АНГЛІЙСЬКА МОВА</p>
+
+								</div>
+
+							</div>
+
+							<div class="element-wrap">
+
+								<img src="/tpl_img/rubik.png" class="main-image" alt="">
+
+								<div>
+
+									<p class="subject-title element-margin-top-20">ТЗНПК</p>
+
+								</div>
+
+							</div>
+
+						</div>
+
+						<div class="sub-section-btns col-xl-12 offset-xl-4 col-lg-12 offset-lg-4 col-md-12 offset-md-1">
+							<a href="" 
+
+							class="inline-block-display button-full-blue button-full-blue--big element-width-300 element-border-radius-5 ">
+
+							ЦІНА ЗА ПІДГОТОВКУ - 2900 грн.
+
+						</a>
+
+						<a href="" 
+
+						class="inline-block-display button-full-white element-width-300">
+
+						ДЕТАЛЬНІШЕ
+
+					</a>
+
+
+				</div>
+			</div>
+
+		</div>
+
+		<div class="how-to-start sub-section" data-section="2">
+
+			<div class="sub-section-element">
+
+				<div class="info-block col-md-12">
+
+					<div>
+
+						Щоб <span class="text-magistr">почати підготовку</span>, необхідно
+
+					</div>
+
+				</div>
+
+				<div class="element-wrap-list elements-block col-md-12 col-sm-12 col-xs-12">
+
+					<div class="element-wrap element-width-200">
+
+						<img src="/tpl_img/register.png" class="main-image" alt="">
+
+						<div>
+
+							<a href="" 
+							class="block-display button-full-orange element-width-200">
+							ЗАРЕЄСТРУВАТИСЯ
+						</a>
+
+					</div>
+
+				</div>
+
+				<div class="element-wrap element-width-200">
+
+					<img src="/tpl_img/contract.png" class="main-image" alt="">
+
+					<div>
+
+						<a href=""  
+						class="block-display button-full-orange button-full-orange--two-lines element-width-200">
+						ОЗНАЙОМИТИСЯ З ДОГОВОРОМ
+					</a>
+
+				</div>
+
+			</div>
+
+			<div class="element-wrap element-width-200">
+
+				<img src="/tpl_img/pay.png" class="main-image" alt="">
+
+				<div>
+
+					<a href="" 
+					class="block-display button-full-orange  element-width-200">
+					СПЛАТИТИ
+				</a>
+
+			</div>
+
+		</div>
+
+		<div class="col-xl-12 offset-xl-8 col-lg-12 offset-lg-8 col-md-5 offset-md-0 element-margin-top-40">
+
+
+			<a href="" 
+
+			class="inline-block-display button-full-white element-width-300 ">
+
+			ДЕТАЛЬНІШЕ
+
+		</a>
+
+
+	</div>
+
+
+
+</div>
+
+</div>
+
+</div>
+
+<div class="how-to-learn sub-section" data-section="3">
+
+	<div class="sub-section-element">
+
+		<div class="info-block col-md-12 ">
+
+			<div>
+
+				<span class="text-magistr">Процес підготовки</span> відбувається в такий спосіб
+
+			</div>
+
+		</div>
+
+		<div class="element-wrap-list-arrowed elements-block col-md-12">
+
+			<div class="element-wrap">
+
+				<div>
+
+					<p class="subject-title element-margin-top-minus-20">Дивіться відеоуроки</p>
+
+				</div>
+
+
+
+				<div class="step-element">
+
+					<img src="/tpl_img/video.png" class="main-image" alt="">
+
+					<img src="/tpl_img/arrow.png" class="main-arrow" alt="">
+
+					<img src="/tpl_img/arrow-down.png" class="twin-arrow" alt="">
+
+				</div>
+
+			</div>
+
+			<div class="element-wrap">
+
+				<div>
+
+					<p class="subject-title element-margin-top-minus-20">Переглядайте конспект</p>
+
+				</div>
+
+				<div class="step-element">
+
+					<img src="/tpl_img/conspect.png" class="main-image" alt="">
+
+					<img src="/tpl_img/arrow.png" class="main-arrow" alt="">
+
+					<img src="/tpl_img/arrow-down.png" class="twin-arrow" alt="">
+
+				</div>
+
+			</div>
+
+			<div class="element-wrap">
+
+				<div>
+
+					<p class="subject-title element-margin-top-minus-20">Вивчайте додаткові матеріали</p>
+
+				</div>
+
+				<div class="step-element">
+
+					<img src="/tpl_img/materials.png" class="main-image" alt="">
+
+					<img src="/tpl_img/arrow.png" class="main-arrow" alt="">
+
+					<img src="/tpl_img/arrow-down.png" class="twin-arrow" alt="">
+
+				</div>
+
+			</div>
+
+			<div class="element-wrap">
+
+				<div>
+
+					<p class="subject-title element-margin-top-minus-20">Виконуйте тести</p>
+
+				</div>
+
+				<div class="step-element">
+
+					<img src="/tpl_img/tests.png" class="main-image" alt="">
+
+					<img src="/tpl_img/arrow.png" class="main-arrow" alt="">
+
+					<img src="/tpl_img/arrow-down.png" class="twin-arrow" alt="">
+
+				</div>
+
+			</div>
+
+			<div class="element-wrap">
+
+				<div>
+
+					<p class="subject-title element-margin-top-minus-20">Слідкуйте за власним прогресом</p>
+
+				</div>
+
+				<div class="step-element">
+
+					<img src="/tpl_img/progress.png" class="main-image" alt="">
+
+				</div>
+
+			</div>
+
+
+		</div>
+
+		
+		<div class="element-wrap--question element-wrap ">
+
+
+			<div class="inline-block-display subject-title element-width-300">
+				А також ставте свої питання досвідченим наставникам
+			</div>
+			<div class="inline-block-display step-element">
+				<img src="/tpl_img/arrow.png" class="main-arrow image-arrow-question" alt="">
+				<img src="/tpl_img/questions.png" class="main-image " alt="">
+				<img src="/tpl_img/arrow-down.png" class="twin-arrow" alt="">
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="why-us sub-section" data-section="4">
+
+	<div class="sub-section-element">
+
+		<div class="info-block col-md-12">
+
+			<div>
+
+				<span class="text-magistr">Чому ми?</span> Можливість займатися
+
+			</div>
+
+		</div>
+
+		<div class="element-wrap-list elements-block col-md-12">
+
+			<div class="element-wrap element-width-200">
+
+				<div>
+
+					<p class="subject-title--blue element-width-200">РЕЗУЛЬТАТИВНО</p>
+
+				</div>
+
+				<img src="/tpl_img/result.png" class="main-image" alt="">
+
+			</div>
+
+			<div class="element-wrap element-width-200">
+
+				<div>
+
+					<p class="subject-title--blue element-width-200">ДЕ ЗАВГОДНО</p>
+
+				</div>
+
+				<img src="/tpl_img/anywhere.png" class="main-image" alt="">
+
+			</div>
+
+			<div class="element-wrap element-width-200">
+
+				<div>
+
+					<p class="subject-title--blue element-width-200">КОЛИ ЗАВГОДНО</p>
+
+				</div>
+
+				<img src="/tpl_img/whenever.png" class="main-image" alt="">
+
+			</div>
+
+			<div class="element-wrap element-width-200">
+
+				<div>
+
+					<p class="subject-title--blue element-width-200">ЕКОНОМНО</p>
+
+				</div>
+
+				<img src="/tpl_img/econom.png" class="main-image" alt="">
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+</div>
+</div>
+</div>
+
+</div>
+
+<div class="container">
+
+	<div class="row">
+
+		<div class="footer">
+
+			<div class="content">
+
+				<div class="element-wrap-list footer-columns col-md-12 element-margin-bottom-40 element-margin-top-40">
+
+					<div class="element-wrap  element-width-200">
+
+						<h3 class="subject-title--orange">
+
+							Інформація
+
+						</h3>
+
+						<ul class="footer-list-block">
+							<li>
+								<a href="">link1 </a>
+							</li>
+							<li>
+								<a href="">link2</a>
+							</li>
+							<li>
+								<a href="">link3</a>
+							</li>
+						</ul>
+
+					</div>
+
+					<div class="element-wrap element-width-200">
+
+						<h3 class="subject-title--orange">
+
+							Про нас
+
+						</h3>
+
+						<ul class="footer-list-block">
+							<li>
+								<a href="">link1 </a>
+							</li>
+							<li>
+								<a href="">link2</a>
+							</li>
+							<li>
+								<a href="">link3</a>
+							</li>
+						</ul>
+
+						<h3 class="subject-title--orange">
+
+							Партнери
+
+						</h3>
+
+						<ul class="footer-list-block">
+							<li>
+								<a href="">link1 </a>
+							</li>
+							<li>
+								<a href="">link2</a>
+							</li>
+							<li>
+								<a href="">link3</a>
+							</li>
+						</ul>
+
+					</div>
+
+					<div class="element-wrap element-width-200">
+
+						<h3 class="subject-title--orange">
+
+							Новини
+
+						</h3>
+
+						<ul class="footer-list-block">
+							<li>
+								<a href="">link1 </a>
+							</li>
+							<li>
+								<a href="">link2</a>
+							</li>
+							<li>
+								<a href="">link3</a>
+							</li>
+						</ul>
+
+						<h3 class="subject-title--orange">
+
+							Блог
+
+						</h3>
+
+						<ul class="footer-list-block">
+							<li>
+								<a href="">link1 </a>
+							</li>
+							<li>
+								<a href="">link2</a>
+							</li>
+							<li>
+								<a href="">link3</a>
+							</li>
+						</ul>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<div class="sub-footer  ">
+
+			<div class="content element-margin-bottom-40 element-margin-top-40">
+				<div class="element-wrap-list footer-columns col-md-12">
+					<div class="element-wrap element-width-350">
+						<div class="sub-footer-text">
+							<p> <a href="" class="link-contacts"> Контакты </a> </p>
+							<p>м. Харків, вул. Плеханівська 18, оф. 816А
+								<div><a href="tel: +380996545428">тел: (099) 654-54-28</a>; <a href="tel: +380733226768">(073) 322-67-68</a></div></p>
+								<p>ТОВ "Онлайн-центр "МагістрЗНО"</p>
+							</div>
+						</div>
+
+						<div class="element-wrap element-width-350">
+							<div class="sub-footer-text">
+								<p><a href="">Email: magistr.zno@gmail.com</a></p>
+								<p><a href="">Telegram: (068) 057-29-02</a>
+									<div class="social">
+										<span class="social-item"><a href="http://fb.com" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></span>
+										<span class="social-item"><a href="http://youtube.com" target="_blank"><i class="fa fa-youtube-square" aria-hidden="true"></i></a></span>
+									</div>
+									<p>
+										<a href="">
+											Популярні запитання
+										</a>
+									</p>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
+		</body>
+
+		</html>
---
+++ tpl_js/main-js.js
@@ -0,0 +1,60 @@
+function initalize_swap(class_toggle, slide_time, twin_selector, max_resolution_for_click_action) {
+	var max_click_resolution = max_resolution_for_click_action || 0;
+	var slider_block    = 1;
+	var class_wrapper   = class_toggle;
+	var amount_elements = $("." + class_toggle).length;
+	var sub_selector    = twin_selector || '';
+	this.time_to_change = slide_time;
+	this.interval_id;
+	this.change_block = function() {
+		$('.' + class_wrapper + "[data-section = " + slider_block + "]").hide(700);
+		if(sub_selector != "") {
+			$(sub_selector + "[data-rel-section = " + slider_block + "]").toggleClass("active");
+		}
+		slider_block++;
+		if(slider_block > amount_elements) slider_block = 1;
+		$('.' + class_wrapper + "[data-section = " + slider_block + "]").show(700);
+		if(sub_selector != "") {
+			$(sub_selector + "[data-rel-section = " + slider_block + "]").toggleClass("active");
+		}
+	}
+	this.clearIntervalMethod = function() {
+		clearInterval(this.interval_id)
+	}
+	this.click_block = function() {
+		$('.' + class_wrapper).hide(100);
+		$('.' + class_wrapper + "[data-section = " + slider_block + "]").show(700);
+		if(sub_selector != "") {
+			$(sub_selector + "[data-rel-section = " + slider_block + "]").toggleClass("active");
+		}
+	}
+	this.setBlockNum = function(block_num) {
+		slider_block = block_num;
+	}
+	this.unsetSelectedBlockClass = function() {
+		if(sub_selector != "") {
+			$(sub_selector + "[data-rel-section = " + slider_block + "]").toggleClass("active");
+		}
+	}
+	this.run = function() {
+		$('.' + class_wrapper).hide(100);
+		$('.' + class_wrapper + "[data-section = " + slider_block + "]").show(700);
+		if(sub_selector != "") {
+			$(sub_selector + "[data-rel-section = " + slider_block + "]").toggleClass("active");
+		}
+		if(window.innerWidth < max_click_resolution) {
+			this.interval_id = setInterval(this.change_block, this.time_to_change)
+		}
+	}
+}
+$(document).ready(function(){
+	var swap_main = new initalize_swap('sub-section', 7000, '.menu .menu-elements li', 767);
+	swap_main.run();
+
+	$(".menu .menu-elements li").click(function(){
+		swap_main.clearIntervalMethod();
+		swap_main.unsetSelectedBlockClass();
+		swap_main.setBlockNum($(this).attr('data-rel-section'));
+		swap_main.click_block();
+	});
+})
