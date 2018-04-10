0015537946482d9cd18727458865550f401e424f
talanovartem <talanovartem@gmail.com>
2018-04-10 23:12:54 +0300

    Talanov: fixed bugs

---
+++ tpl_blocks/header-registration.php
@@ -0,0 +1,66 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once(BLOCK_FOLDER . "/head.php"); ?>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="header">
+				<div class="header-info">
+					<div class="content">
+						<div class="col-md-12">
+
+							<div class="header-begin-info">
+								<div class="content">
+									<div class="col-md-12">
+										<img src="/tpl_img/logo.png" alt="" class="block-display logo">
+										<div class="header-begin-info-btns">
+											<a href="" 
+											class="block-display button-border-blue element-width-240 element-border-radius-5 ">
+											СПРОБУВАТИ БЕЗКОШТОВНО
+										</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="clear"></div>
+					<div class="menu-desktop">
+						<div class="menu">
+							<div class="content">
+								<ul class="menu-elements">
+									<li data-rel-section="1">ГОЛОВНЕ</li>
+									<li data-rel-section="2">ЯК РОЗПОЧАТИ</li>
+									<li data-rel-section="3">ЯК НАВЧАТИСЯ</li>
+									<li data-rel-section="4">ЧОМУ МИ?</li>
+								</ul>
+								<div class="clear"></div>
+							</div>
+						</div>
+					</div>
+					<div class="menu-mobile">
+						<div class="menu">
+							<div class="content">
+								<ul class="menu-elements">
+									<li data-rel-section="1">
+										<div id="m-btn-main" class="menu-mobile-btn-main"></a>
+										</li>
+										<li data-rel-section="2">
+											<div id="m-btn-start" class="menu-mobile-btn-start"></a>
+											</li>
+											<li data-rel-section="3">
+
+												<div id="m-btn-learn" class="menu-mobile-btn-learn"></a>
+
+												</li>
+												<li data-rel-section="4">
+
+													<div id="m-btn-adv" class="menu-mobile-btn-adv"></a>
+
+													</ul>
+													<div class="clear"></div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
