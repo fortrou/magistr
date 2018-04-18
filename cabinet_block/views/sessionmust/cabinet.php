4cb56768e853662f563a5485727647b002b74e95
DESKTOP-BVMM1CB\fortrou <fortrou@gmail.com>
2018-04-18 13:05:24 +0300

    cabinet header markup

--- cabinet_block/views/sessionmust/cabinet.php
+++ cabinet_block/views/sessionmust/cabinet.php
@@ -1,4 +1,52 @@
-<?php require_once(BLOCK_FOLDER . "/header.php"); ?>
+<?php //require_once(BLOCK_FOLDER . "/header.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<?php require_once(BLOCK_FOLDER . "/head.php"); ?>
+<body>
+	<div class="header">
+	<div class="menu-desktop">
+		<div class="menu">
+			<div class="content">
+				<ul class="menu-elements">
+					<li>
+						<a href="#">
+							<img src="/tpl_img/logo-mini.png" alt="mini-logo">
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="/tpl_img/calendar.png" alt="calendar">
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="/tpl_img/homework.png" alt="homework">
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="/tpl_img/messages.png" alt="messages">
+						</a>
+					</li>
+					<li>
+						<a href="">Курс
+						</a>
+					</li>
+					<li>
+						<a href="">
+						Профіль
+						</a>
+						<img src="/tpl_img/sort_down.png" alt="">
+
+					</li>
+				</ul>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+	</div>
+
 	<div class="container">
 		<div class="row">
 
